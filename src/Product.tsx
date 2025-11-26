a8721c3dc8336cc712136a7892b0d2d2f61007ba
SerjRoman <sergeyroman26@gmail.com>
2025-11-26 19:38:56 +0000

    Styles and counter state

--- src/Product.tsx
+++ src/Product.tsx
@@ -1,34 +1,36 @@
+import { useState } from 'react'
+import styles from './product.module.css'
 
-
-
-// export function Product(props: {title: string, price: number}) {
-//     return <div>
-//         <h3>Title: {props.title}</h3>
-//         <p>Price: {props.price}</p>
-//     </div>
-// }
-
-
-// export function Product(props: ProductProps) {
-//     return <div>
-//         <h3>Title: {props.title}</h3>
-//         <p>Price: {props.price}</p>
-//     </div>
-// }
-// export function Product({ title, price, price1, price2, price3}: ProductProps) {
-//     return <div>
-//         <h3>Title: {title}</h3>
-//         <p>Price: {price}</p>
-//     </div>
-// }
 interface ProductProps {
     title: string,
     price: number,
+    image: string
 }
+
 export function Product(props: ProductProps) {
-    const {title, price} = props
-    return <div>
-        <h3>Title: {title}</h3>
-        <p>Price: {price}</p>
+    const {title, price, image} = props
+    const [counter, setCounter] = useState(0)
+    function incrementProduct(){
+        if (counter < 100){
+            setCounter(counter + 1)
+        }
+    }
+     function decrementProduct(){
+        if (counter > 0){
+            setCounter(counter - 1)
+        }
+    }
+
+    return <div className={styles.block}>
+        <img className = {styles.image} src = {image} alt=''/>
+        <p className = {styles.title}>Title: {title}</p>
+        <p className = {styles.price}>Price: {price}</p>
+
+        <div className={styles.counterBlock}>
+            <p className = {styles.counter}>Count: { counter }</p>
+            <button className = {styles.counterButton} onClick={incrementProduct}>&#43;</button>
+            <button className = {styles.counterButton} onClick={decrementProduct}>&#8722;</button>
+        </div>
     </div>
+
 }
--- src/ProductList.tsx
+++ src/ProductList.tsx
@@ -2,12 +2,12 @@ import { Product } from "./Product"
 
 export function ProductList() {
     const data = [
-            {title: "Product 1", price: 950}, // 0
-            {title: "Product 2", price: 850}, // 1
-            {title: "Product 3", price: 6750}, // 2
-            {title: "Product 4", price: 5503},
-            {title: "Product 5", price: 520},
-            {title: "Product 6", price: 150},
+            {title: "Product 1", price: 950, image: "https://cdn.pixabay.com/photo/2024/02/28/07/42/european-shorthair-8601492_1280.jpg"}, // 0
+            {title: "Product 2", price: 850, image: "https://cdn.pixabay.com/photo/2024/02/28/07/42/european-shorthair-8601492_1280.jpg"}, // 1
+            {title: "Product 3", price: 6750, image: "https://cdn.pixabay.com/photo/2024/02/28/07/42/european-shorthair-8601492_1280.jpg"}, // 2
+            {title: "Product 4", price: 5503, image: "https://cdn.pixabay.com/photo/2024/02/28/07/42/european-shorthair-8601492_1280.jpg"},
+            {title: "Product 5", price: 520, image: "https://cdn.pixabay.com/photo/2024/02/28/07/42/european-shorthair-8601492_1280.jpg"},
+            {title: "Product 6", price: 150, image: "https://cdn.pixabay.com/photo/2024/02/28/07/42/european-shorthair-8601492_1280.jpg"},
         ]
     return <div>
         {/*
@@ -20,6 +20,7 @@ export function ProductList() {
             return <Product 
                 title={product.title} 
                 price={product.price}
+                image={product.image}
                 // НЕ передаем в key ИНДЕКСЫ МАССИВОВ
                 key={product.title}
             ></Product>
